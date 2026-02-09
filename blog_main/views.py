72b46a4292fe643bb1246b2a8281e520c51b5f4d
gt951436 <tyagigarv95@gmail.com>
2026-02-09 16:55:14 +0530

    showing categories on home page

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from blogs.models import Category
 
 def home(request):
     #return HttpResponse('<h2>This is Blog App Home Page!</h2>')
-    return render(request,'home.html')
+    categories = Category.objects.all()
+    print(categories)
+    context = {
+        'categories':categories,
+    }
+    return render(request,'home.html',context)
