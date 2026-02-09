28851b647a092b0bde95f9594a20538f498d8f64
gt951436 <tyagigarv95@gmail.com>
2026-02-09 17:56:22 +0530

    displaying non-featured posts

--- blog_main/views.py
+++ blog_main/views.py
@@ -4,11 +4,12 @@ from blogs.models import Category,Blog
 def home(request):
     #return HttpResponse('<h2>This is Blog App Home Page!</h2>')
     categories = Category.objects.all()
-    #print(categories)
-    featured_posts = Blog.objects.filter(is_featured = True)
-    print(featured_posts)
+    featured_posts = Blog.objects.filter(is_featured = True,status = "Published")
+    posts = Blog.objects.filter(is_featured=False,status = "Published")
+    #print(posts)
     context = {
         'categories':categories,
         'featured_posts':featured_posts,
+        'posts':posts,
     }
     return render(request,'home.html',context)
