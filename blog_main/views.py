c92f713c7a0a8f7522b64e3f5bcf5bb3b9578695
gt951436 <tyagigarv95@gmail.com>
2026-02-09 17:36:26 +0530

    displaying featureed posts

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from blogs.models import Category
+from blogs.models import Category,Blog
 
 def home(request):
     #return HttpResponse('<h2>This is Blog App Home Page!</h2>')
     categories = Category.objects.all()
-    print(categories)
+    #print(categories)
+    featured_posts = Blog.objects.filter(is_featured = True)
+    print(featured_posts)
     context = {
         'categories':categories,
+        'featured_posts':featured_posts,
     }
     return render(request,'home.html',context)
